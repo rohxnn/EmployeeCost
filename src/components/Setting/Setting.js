b0891672c4679c4c1fb05f24c47c3cd8e4892d66
rohxnn <rohanreji123@gmail.com>
2024-05-10 09:46:57 +0530

    feat(Setting): add component and logic

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Setting.css';
 
+function Setting({ totalAmount, setCostPerPerson, setProfitPercentage }) {
+    const [employeeNumber, setEmployeeNumber] = useState(1);
 
-function Setting({ totalAmount }) {
-const [employeeNumber, setEmployeeNumber] = useState(1);
-const [profitPercent, setProfitPercent] = useState(100);
+    useEffect(() => {
+        const costPerPerson = totalAmount / employeeNumber;
+        setCostPerPerson(costPerPerson);
+        console.log(costPerPerson)
+    }, [totalAmount, employeeNumber]);
+
+    const handleEmployeeNumberChange = (e) => {
+        setEmployeeNumber(e.target.value);
+    }
+
+    const handleProfitChanges = (e) => {
+        setProfitPercentage(e.target.value);
+    }
 
     return (
-        <div>
-            <h1>Setting
-                {totalAmount}
-            </h1>
+        <div className='settingForm'>
+            <input type='number' name="employeeNumber" onChange={(e) => handleEmployeeNumberChange(e)} />
+            <input type='number' name='profitPercent' onChange={(e) => handleProfitChanges(e)} />
         </div>
-    )
+    );
 }
 
 export default Setting;
