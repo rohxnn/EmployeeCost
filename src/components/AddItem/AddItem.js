69020852cab7c64fd4958b9cc9457569b2d13927
rohxnn <rohanreji123@gmail.com>
2024-05-10 10:18:44 +0530

    refacttor(AddItem): remove unwanted logs

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -8,13 +8,12 @@ function AddItem() {
   const [inputList, setInputList] = useState([{ description: '', amount: '', month: '' }]);
   const [costPerPerson, setCostPerPerson] = useState(0);
   const [profitPercent, setProfitPercentage] = useState(0);
+
   const handleInputChange = (e, index) => {
     const { name, value } = e.target;
     const list = [...inputList];
     list[index][name] = value;
     setInputList(list);
-    console.log(profitPercent);
-    console.log(costPerPerson)
   }
 
   const handleAddClick = () => {
@@ -22,12 +21,10 @@ function AddItem() {
   }
 
   function getTotalAmount() {
-    console.log('hiii')
     const sum = (accumulator, data) => {
       return accumulator + (data.amount / data.month);
     };
     const totalAmount = inputList.reduce(sum, 0);
-    console.log(totalAmount);
     return totalAmount.toFixed(2);
   }
 
@@ -54,12 +51,12 @@ function AddItem() {
             <EmployeeDetail
               costPerPerson={costPerPerson}
               profitPercent={profitPercent}
-              totalAmount={getTotalAmount()}
+              totalAmount={getTotalAmount}
             ></EmployeeDetail>
           </div>
         </div>
         <div className='side-box'>
-          <Setting totalAmount={getTotalAmount()}
+          <Setting totalAmount={getTotalAmount}
             setCostPerPerson={setCostPerPerson}
             setProfitPercentage={setProfitPercentage}
           ></Setting>
