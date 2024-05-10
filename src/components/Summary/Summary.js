12a082d9798ea57bacbcd59c19d1bbf17b3cac85
rohxnn <rohanreji123@gmail.com>
2024-05-10 15:33:35 +0530

    feat(Summary): display all the values

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Summary.css'
 
-function Summary() {
-
+function Summary(props) {
+    return (
+        <div>
+            <p>Total Cost - Rs.{isNaN(props.totalAmount) || !isFinite(props.totalAmount) ? 0 : props.totalAmount.toFixed(2)}</p>
+            <p>Cost Per Person - Rs.{isNaN(props.costPerPerson) || !isFinite(props.costPerPerson) ? 0 : props.costPerPerson.toFixed(2)}</p>
+            <p>Total Cost Per Person - Rs.{isNaN(props.totalCostPerEmployee) || !isFinite(props.totalCostPerEmployee) ? 0 : props.totalCostPerEmployee.toFixed(2)}</p>
+            <p>Profit - Rs.{isNaN(props.profit) || !isFinite(props.profit) ? 0 : props.profit.toFixed(2)}</p>
+            <p>CashFlow Per Month : Rs.0.00</p>
+        </div>
+    )
 }
 
 export default Summary;
