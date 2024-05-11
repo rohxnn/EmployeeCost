67229daa3860e5a6f9155d2a44e3fdfc947b7a66
rohxnn <rohanreji123@gmail.com>
2024-05-11 22:57:44 +0530

    style(Setting): style ui for setting

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -18,9 +18,31 @@ function Setting(props) {
     }
 
     return (
-        <div className='settingForm'>
-            <input type='number' name="employeeNumber" onChange={(e) => handleEmployeeNumberChange(e)} />
-            <input type='number' name='profitPercent' onChange={(e) => handleProfitChanges(e)} />
+        <div>
+            <div className='setting-header'>
+                Settings
+            </div>
+            <div className='settingForm'>
+                <label>Number of Empolyees:</label>
+                <input type='number' name="employeeNumber" onChange={(e) => handleEmployeeNumberChange(e)} />
+                <label>Profit Percentage:</label>
+                <input type='number' name='profitPercent' onChange={(e) => handleProfitChanges(e)} />
+                <label>Other:</label>
+                <div className='toggle-display'>
+                    <label className="switch">
+                        <input type="checkbox" />
+                        <span className="slider round"></span>
+                    </label>
+                    <label style={{ marginLeft: '10px' }}>Add GST</label>
+                </div>
+                <div className='toggle-display'>
+                    <label className="switch">
+                        <input type="checkbox" />
+                        <span className="slider round"></span>
+                    </label>
+                    <label style={{ marginLeft: '10px' }}>Reduce TDS</label>
+                </div>
+            </div>
         </div>
     );
 }
