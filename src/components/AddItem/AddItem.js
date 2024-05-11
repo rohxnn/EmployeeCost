2035dfe9261aad32fbc6523eaa701f6b6794d957
rohxnn <rohanreji123@gmail.com>
2024-05-11 22:57:16 +0530

    style(AppItem): style ui for add item

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -19,7 +19,7 @@ function AddItem() {
   }
 
   const handleAddClick = () => {
-    setInputList([...inputList, { description: '', amount: '', month: '' }])
+    setInputList([...inputList, { description: '', amount: '', occurence: '' }])
   }
 
   // function getTotalAmount() {
@@ -41,49 +41,43 @@ function AddItem() {
 
   useEffect(() => {
     const newTotalAmount = inputList.reduce((accumulator, data) => {
-      return accumulator + (data.amount / data.month);
+      return accumulator + (data.amount / data.occurence);
     }, 0);
     console.log(newTotalAmount);
     setTotalAmount(newTotalAmount);
   }, [inputList]);
-
   return (
     <>
       <div className='box'>
         <div className='sub-box'>
-          {
-            updatedInputList.map((x, i) => {
-              return (
-                <div className='formBox' key={i}>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Description</th>
-                      <th>Amount</th>
-                      <th>Month</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>
-                      <input type='text' name="description" value={x.description} onChange={(e) => handleInputChange(e, i)} />
-                        </td>
-                      <td>
-                      <input type='number' name="amount" value={x.amount} onChange={(e) => handleInputChange(e, i)} />
-                        </td>
-                      <td>
-                      <input type='number' name="month" value={x.month} onChange={(e) => handleInputChange(e, i)} />
-                        </td>
-                        <button disabled={updatedInputList.length <= 1} onClick={() => deleteSection(i)}>X</button>
-                    </tr>
-                  </tbody>
-                </table>
-                </div>
-              );
-            })
-          }
-          <div>
-            <button onClick={handleAddClick}>Add Items</button>
+          <table>
+            <thead className="myTable">
+              <tr className="table-header">
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Occurence</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {updatedInputList.map((x, i) => (
+                <tr key={i}>
+                  <td>
+                    <input className='custom-input' type='text' name="description" value={x.description} onChange={(e) => handleInputChange(e, i)} />
+                  </td>
+                  <td>
+                    <input className='custom-input' type='number' name="amount" value={x.amount} onChange={(e) => handleInputChange(e, i)} />
+                  </td>
+                  <td className='monthly-block'>
+                    <input className='custom-input monthly-input' type='number' name="occurence" placeholder='monthly' value={x.month} onChange={(e) => handleInputChange(e, i)} />
+                    <button disabled={updatedInputList.length <= 1} className='delete-button' onClick={() => deleteSection(i)}>X</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className='button-box'>
+            <button className='add-button' onClick={handleAddClick}>Add Items</button>
           </div>
           <div>
             <EmployeeDetail
@@ -104,5 +98,4 @@ function AddItem() {
     </>
   );
 }
-
 export default AddItem;
