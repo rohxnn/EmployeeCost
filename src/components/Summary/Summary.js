68cd9ae958f56b9abfc88976891a721293f26671
rohxnn <rohanreji123@gmail.com>
2024-05-13 15:26:54 +0530

    refactor(Summary): use employee  context

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import './Summary.css'
+import { useEmployeeContext } from '../../controller/UserContext';
 
-function Summary(props) {
+function Summary() {
+    const {
+        employeeNumber,
+        profitPercent,
+        totalAmount,
+        gstAmount,
+        salary
+    } = useEmployeeContext();
+
+    const gstTotalAmount = totalAmount + gstAmount;
+    const costPerEmployee = gstTotalAmount / employeeNumber;
+    const totalCostPerEmployee = costPerEmployee + Number(salary);
+    const profit = totalCostPerEmployee * (profitPercent / 100);
+    function c() {
+        console.log('c', employeeNumber, totalAmount, salary, profit, gstAmount, gstTotalAmount);
+    }
+
     return (
         <div>
-            <p>Total Cost - Rs.{isNaN(props.totalAmount) || !isFinite(props.totalAmount) ? 0 : props.totalAmount.toFixed(2)}</p>
-            <p>Cost Per Person - Rs.{isNaN(props.costPerPerson) || !isFinite(props.costPerPerson) ? 0 : props.costPerPerson.toFixed(2)}</p>
-            <p>Total Cost Per Person - Rs.{isNaN(props.totalCostPerEmployee) || !isFinite(props.totalCostPerEmployee) ? 0 : props.totalCostPerEmployee.toFixed(2)}</p>
-            <p>Profit - Rs.{isNaN(props.profit) || !isFinite(props.profit) ? 0 : props.profit.toFixed(2)}</p>
+            {c()}
+            <p>Total Cost - Rs.{isNaN(gstTotalAmount) || !isFinite(gstTotalAmount) ? 0 : gstTotalAmount.toFixed(2)}</p>
+            <p>Cost Per Person - Rs.{isNaN(costPerEmployee) || !isFinite(costPerEmployee) ? 0 : costPerEmployee.toFixed(2)}</p>
+            <p>Total Cost Per Person - Rs.{isNaN(totalCostPerEmployee) || !isFinite(totalCostPerEmployee) ? 0 : totalCostPerEmployee}</p>
+            <p>Profit - Rs.{isNaN(profit) || !isFinite(profit) ? 0 : profit.toFixed(2)}</p>
             <p>CashFlow Per Month : Rs.0.00</p>
         </div>
     )
