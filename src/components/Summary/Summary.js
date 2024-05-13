5967b83ed7e99bdde43aaa980968b1ea196e0408
rohxnn <rohanreji123@gmail.com>
2024-05-13 15:37:21 +0530

    refactor(Summary): code clean

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Summary.css'
+//context
 import { useEmployeeContext } from '../../controller/UserContext';
 
 function Summary() {
@@ -15,16 +16,12 @@ function Summary() {
     const costPerEmployee = gstTotalAmount / employeeNumber;
     const totalCostPerEmployee = costPerEmployee + Number(salary);
     const profit = totalCostPerEmployee * (profitPercent / 100);
-    function c() {
-        console.log('c', employeeNumber, totalAmount, salary, profit, gstAmount, gstTotalAmount);
-    }
-
+    
     return (
         <div>
-            {c()}
             <p>Total Cost - Rs.{isNaN(gstTotalAmount) || !isFinite(gstTotalAmount) ? 0 : gstTotalAmount.toFixed(2)}</p>
             <p>Cost Per Person - Rs.{isNaN(costPerEmployee) || !isFinite(costPerEmployee) ? 0 : costPerEmployee.toFixed(2)}</p>
-            <p>Total Cost Per Person - Rs.{isNaN(totalCostPerEmployee) || !isFinite(totalCostPerEmployee) ? 0 : totalCostPerEmployee}</p>
+            <p>Total Cost Per Person - Rs.{isNaN(totalCostPerEmployee) || !isFinite(totalCostPerEmployee) ? 0 : totalCostPerEmployee.toFixed(2)}</p>
             <p>Profit - Rs.{isNaN(profit) || !isFinite(profit) ? 0 : profit.toFixed(2)}</p>
             <p>CashFlow Per Month : Rs.0.00</p>
         </div>
