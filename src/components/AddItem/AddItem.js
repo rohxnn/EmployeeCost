9d4209312c82d58bb651828b6e8559393fa1bf4b
rohxnn <rohanreji123@gmail.com>
2024-05-13 09:14:28 +0530

    style(AddItem): style AddItem

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -9,7 +9,6 @@ function AddItem() {
   const [costPerPerson, setCostPerPerson] = useState(0);
   const [profitPercent, setProfitPercentage] = useState(0);
   const [totalAmount, setTotalAmount] = useState(0);
-  let updatedInputList = [...inputList];
 
   const handleInputChange = (e, index) => {
     const { name, value } = e.target;
@@ -32,18 +31,15 @@ function AddItem() {
 
   function deleteSection(i) {
     console.log(i);
-    updatedInputList = [...inputList];
+    const updatedInputList = [...inputList];
     updatedInputList.splice(i, 1);
     setInputList(updatedInputList);
-    console.log(updatedInputList);
-    console.log(inputList);
   }
 
   useEffect(() => {
     const newTotalAmount = inputList.reduce((accumulator, data) => {
       return accumulator + (data.amount / data.occurence);
     }, 0);
-    console.log(newTotalAmount);
     setTotalAmount(newTotalAmount);
   }, [inputList]);
   return (
@@ -60,7 +56,7 @@ function AddItem() {
             </thead>
 
             <tbody>
-              {updatedInputList.map((x, i) => (
+              {inputList.map((x, i) => (
                 <tr key={i}>
                   <td>
                     <input className='custom-input' type='text' name="description" value={x.description} onChange={(e) => handleInputChange(e, i)} />
@@ -69,8 +65,8 @@ function AddItem() {
                     <input className='custom-input' type='number' name="amount" value={x.amount} onChange={(e) => handleInputChange(e, i)} />
                   </td>
                   <td className='monthly-block'>
-                    <input className='custom-input monthly-input' type='number' name="occurence" placeholder='monthly' value={x.month} onChange={(e) => handleInputChange(e, i)} />
-                    <button disabled={updatedInputList.length <= 1} className='delete-button' onClick={() => deleteSection(i)}>X</button>
+                    <input className='custom-input monthly-input' type='number' name="occurence" value={x.occurence} placeholder='monthly' onChange={(e) => handleInputChange(e, i)} />
+                    <button disabled={inputList.length <= 1} className='delete-button' onClick={() => deleteSection(i)}>X</button>
                   </td>
                 </tr>
               ))}
@@ -79,13 +75,13 @@ function AddItem() {
           <div className='button-box'>
             <button className='add-button' onClick={handleAddClick}>Add Items</button>
           </div>
-          <div>
+          <>
             <EmployeeDetail
               costPerPerson={costPerPerson}
               profitPercent={profitPercent}
               totalAmount={totalAmount}
             ></EmployeeDetail>
-          </div>
+          </>
         </div>
         <div className='side-box'>
           <Setting
